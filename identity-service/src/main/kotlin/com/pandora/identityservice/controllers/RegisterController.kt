9de3e9f4986c21169a2d492b259107e48f3c11eb
domeeno <dominicflocea.dev@gmail.com>
2021-10-26 04:18:49 +0300

    add jwt based login

--- identity-service/src/main/kotlin/com/pandora/identityservice/controllers/RegisterController.kt
+++ identity-service/src/main/kotlin/com/pandora/identityservice/controllers/RegisterController.kt
@@ -1,17 +1,19 @@
 package com.pandora.identityservice.controllers
 
+import com.pandora.identityservice.dto.LoginDTO
 import com.pandora.identityservice.dto.RegisterDTO
 import com.pandora.identityservice.models.User
 import com.pandora.identityservice.services.UserService
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("auth")
 class RegisterController(private val userService: UserService){
 
     private val logger = LoggerFactory.getLogger(RegisterController::class.java)
@@ -24,7 +26,19 @@ class RegisterController(private val userService: UserService){
             logger.info("registration for ${user.email} succeeded")
             ResponseEntity(dto, HttpStatus.OK)
         } catch (e: Exception) {
-            ResponseEntity("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR)
+            ResponseEntity("Something went wrong: " + e.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody dto: LoginDTO) : ResponseEntity<*> {
+        return try {
+            logger.info("login with " + (dto.email ?: dto.username))
+            val jwt = userService.authenticate(dto);
+            logger.info("login with " + (dto.email ?: dto.username) + " successful")
+            ResponseEntity(jwt, HttpStatus.OK)
+        } catch (e: Exception) {
+            ResponseEntity("login with " + (dto.email ?: dto.username) + " unsuccessful: " + e.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 }
---
+++ identity-service/src/main/kotlin/com/pandora/identityservice/dto/LoginDTO.kt
@@ -0,0 +1,7 @@
+package com.pandora.identityservice.dto
+
+data class LoginDTO (
+    val email: String?,
+    val username: String?,
+    val password: String?,
+)
