5dc2679416d17e56b2413c5c7344c36a6d95070d
domeeno <dominicflocea.dev@gmail.com>
2021-11-01 23:17:37 +0200

    basic rest foundation

--- pricing-service/go.mod
+++ pricing-service/go.mod
@@ -1,3 +1,8 @@
 module pricing-service
 
 go 1.17
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- pricing-service/main.go
+++ pricing-service/main.go
@@ -4,15 +4,50 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
-func handleInfo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am pricing microservice")
+type userPricingDTO struct {
+	userId         uuid.UUID `json:userId`
+	planId         uuid.UUID `json:planId`
+	previousPlanId uuid.UUID `json:previousPlanId`
+	planExp        time.Time `json:planExp`
+}
+
+type userPricings []userPricingDTO
+
+func getInfo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "I am the pricing microservice")
+}
+
+func getPricing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "get pricing by id")
+}
+
+func createPricing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "get pricing by id")
+}
+
+func updatePricing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "get pricing by id")
+}
+
+func handlePricing(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Creating pricing")
 }
 
 func handleRequests() {
-	http.HandleFunc("/info", handleInfo)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/info", getInfo)
+	router.HandleFunc("/pricing/{id}", getPricing).Methods("GET")
+	router.HandleFunc("/pricing/{id}", createPricing).Methods("POST")
+	router.HandleFunc("/pricing/{id}", updatePricing).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
 
 func main() {
