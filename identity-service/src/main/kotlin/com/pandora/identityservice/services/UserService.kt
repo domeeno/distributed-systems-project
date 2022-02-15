72c5f30803de5f4a3bf6efaf886d0304750aef42
domeeno <dominicflocea.dev@gmail.com>
2022-02-15 23:14:37 +0200

    add password recovery

--- identity-service/src/main/kotlin/com/pandora/identityservice/services/UserService.kt
+++ identity-service/src/main/kotlin/com/pandora/identityservice/services/UserService.kt
@@ -2,10 +2,13 @@ package com.pandora.identityservice.services
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.pandora.identityservice.dto.LoginDTO
+import com.pandora.identityservice.dto.RecoveryDTO
 import com.pandora.identityservice.dto.RegisterDTO
+import com.pandora.identityservice.dto.ResetDTO
 import com.pandora.identityservice.models.User
 import com.pandora.identityservice.repositories.SubscriptionRepository
 import com.pandora.identityservice.repositories.UserRepository
+import io.jsonwebtoken.JwtBuilder
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.slf4j.LoggerFactory
@@ -23,10 +26,13 @@ class UserService(
     @Value("\${spring.jwt.secret-key}")
     private val secretKey: String? = "secret"
 
+    @Value("\${spring.services.url.front-end")
+    private val url: String? = "http://localhost:3000"
+
     val passwordEncoder = BCryptPasswordEncoder()
     private val logger = LoggerFactory.getLogger(UserService::class.java)
 
-    fun userCreate (dto: RegisterDTO) : User {
+    fun userCreate (dto: RegisterDTO) : String {
         val user = User();
         user.username = dto.username
         user.email = dto.email
@@ -36,7 +42,10 @@ class UserService(
         user.dateOfBirth = dto.dateOfBirth
         val savedUser = userRepository.save(user)
         subscriptionRepository.createBaseSubscription(savedUser.userId.toString())
-        return user;
+
+        val jwt = generateJwt(user)
+
+        return jwt.compact();
     }
 
     fun authenticate(dto: LoginDTO) : String {
@@ -47,20 +56,64 @@ class UserService(
         }
 
         if (user == null) {
-            logger.warn("User with ${if (dto.email != null) "email " else "username "}" + (dto.email ?: dto.username) + " not found")
+            logger.warn(
+                "User with ${if (dto.email != null) "email " else "username "}" + (dto.email
+                    ?: dto.username) + " not found"
+            )
             throw Exception("Wrong username or password")
         }
 
-        val claims : HashMap<String, Any?> = HashMap<String, Any?>();
-        claims["username"] = user.username;
-        claims["scope"] = "default";
+        val jwt = generateJwt(user)
+
+        return if (passwordEncoder.matches(
+                dto.password,
+                user.password
+            )
+        ) jwt.compact() else throw Exception("Wrong username or password")
+    }
+
+    fun recovery(dto: RecoveryDTO): String {
+        val user = if (dto.email != null) {
+            userRepository.findByEmail(dto.email)
+        } else {
+            userRepository.findByUsername(dto.username)
+        }
+
+        if (user == null) {
+            logger.warn(
+                "User with ${if (dto.email != null) "email " else "username "}" + (dto.email
+                    ?: dto.username) + " not found"
+            )
+            throw Exception("Wrong username or password")
+        }
+
+        val claims: HashMap<String, Any?> = HashMap<String, Any?>();
+        claims["email"] = user.email;
+        claims["reset"] = true;
 
         val jwt = Jwts.builder()
-            .setIssuer(user.userId.toString())
+            .setIssuer("identity")
             .setExpiration(Date(System.currentTimeMillis() + 60 * 60 + 1000)) //hour for now
             .setClaims(claims)
             .signWith(SignatureAlgorithm.HS256, secretKey)
 
-        return if (passwordEncoder.matches(dto.password, user.password)) jwt.compact() else throw Exception("Wrong username or password")
+        return "${url}/confirm/$jwt"
+    }
+
+    fun reset(dto: ResetDTO) {
+
+    }
+
+    private fun generateJwt(user: User): JwtBuilder {
+        val claims: HashMap<String, Any?> = HashMap<String, Any?>();
+        claims["username"] = user.username;
+        claims["email"] = user.email;
+        claims["scope"] = "default";
+
+        return Jwts.builder()
+            .setIssuer("identity")
+            .setExpiration(Date(System.currentTimeMillis() + 60 * 60 + 1000)) //hour for now
+            .setClaims(claims)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
     }
 }
