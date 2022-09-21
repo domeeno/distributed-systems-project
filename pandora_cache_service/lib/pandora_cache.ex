15bd176dca039bdbde9577b0087673e305b7f9a9
domeeno <dominicflocea.dev@gmail.com>
2022-09-21 12:37:51 +0300

    init cache server. remove outdated functionality

---
+++ pandora_cache_service/lib/pandora_cache.ex
@@ -0,0 +1,18 @@
+defmodule PandoraCache do
+  @moduledoc """
+  Documentation for `PandoraCache`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PandoraCache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ pandora_cache_service/test/pandora_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule PandoraCacheTest do
+  use ExUnit.Case
+  doctest PandoraCache
+
+  test "greets the world" do
+    assert PandoraCache.hello() == :world
+  end
+end
