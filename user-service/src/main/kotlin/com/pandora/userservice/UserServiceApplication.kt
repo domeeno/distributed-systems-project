95abd68603531e90694e46e2a72955529749aacf
domeeno <dominicflocea.dev@gmail.com>
2022-09-27 10:25:37 +0300

    init user service

---
+++ user-service/src/main/kotlin/com/pandora/userservice/UserServiceApplication.kt
@@ -0,0 +1,1 @@
+package com.pandora.userserviceimport org.springframework.boot.autoconfigure.SpringBootApplicationimport org.springframework.boot.runApplication@SpringBootApplicationclass UserServiceApplicationfun main(args: Array<String>) {	runApplication<UserServiceApplication>(*args)}
