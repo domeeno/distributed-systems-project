dc891a00f159e481f83f6fabe3a7a7ce25293ea0
domeeno <dominicflocea.dev@gmail.com>
2022-10-03 14:34:32 +0300

    add needed dependencies. add user document

--- course-service/build.gradle.kts
+++ course-service/build.gradle.kts
@@ -23,7 +23,11 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("commons-io:commons-io:2.11.0")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("com.ninja-squad:springmockk:3.1.1")
+	implementation("org.springframework.data:spring-data-mongodb:3.4.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
