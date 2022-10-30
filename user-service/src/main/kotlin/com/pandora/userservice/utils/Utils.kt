55e0932a8869a74cdf6f1fce5277a588dc6bbad2
domeeno <dominicflocea.dev@gmail.com>
2022-10-30 21:30:24 +0200

    lint. fix missing application properties vars

--- user-service/src/main/kotlin/com/pandora/userservice/utils/Utils.kt
+++ user-service/src/main/kotlin/com/pandora/userservice/utils/Utils.kt
@@ -4,42 +4,39 @@ import com.pandora.userservice.models.User
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.apache.commons.io.IOUtils
-import org.aspectj.lang.ProceedingJoinPoint
-import org.aspectj.lang.annotation.Around
-import org.aspectj.lang.annotation.Aspect
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.ResourceLoader
-import org.springframework.data.crossstore.ChangeSetPersister
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.client.HttpClientErrorException.Unauthorized
-import org.springframework.web.server.ResponseStatusException
 import java.io.IOException
 import java.util.Date
-import javax.servlet.http.HttpServletRequest
 
+@Component
+class Utils {
+    @Value("\${security.jwt.secret}")
+    private val secret: String = ""
 
-@Value("\${spring.jwt.secret-key}")
-private val secretKey: String = "secret"
+    @Value("\${server.port}")
+    private val port: String = ""
 
-@Throws(IOException::class)
-fun readResourceIntoString(resourceLoader: ResourceLoader, file: String): String {
-    val resource = resourceLoader.getResource(file)
-    return IOUtils.toString(resource.inputStream, "UTF-8")
-}
+    @Throws(IOException::class)
+    fun readResourceIntoString(resourceLoader: ResourceLoader, file: String): String {
+        val resource = resourceLoader.getResource(file)
+        return IOUtils.toString(resource.inputStream, "UTF-8")
+    }
 
-fun generateJwt(user: User): String {
-    val claims: HashMap<String, Any?> = HashMap()
-    claims["email"] = user.email
-    claims["userId"] = user.userId
-    claims["likes"] = user.likedId
-    claims["saves"] = user.savedId
-    claims["subjects"] = user.subjectsId
+    fun generateJwt(user: User): String {
+        val claims: HashMap<String, Any?> = HashMap()
+        claims["email"] = user.email
+        claims["userId"] = user.userId
+        claims["likes"] = user.likedId
+        claims["saves"] = user.savedId
+        claims["subjects"] = user.subjectsId
 
-    return Jwts.builder()
-        .setIssuer("identity")
-        .setExpiration(Date(System.currentTimeMillis() + 60 * 60 + 1000)) // hour from now (for now)
-        .setClaims(claims)
-        .signWith(SignatureAlgorithm.HS256, secretKey)
-        .compact()
+        return Jwts.builder()
+            .setIssuer("identity:$port")
+            .setExpiration(Date(System.currentTimeMillis() + 60 * 60 + 1000)) // hour from now (for now)
+            .setClaims(claims)
+            .signWith(SignatureAlgorithm.HS256, secret)
+            .compact()
+    }
 }
