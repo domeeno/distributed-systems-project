587be61bcf3fb6e5843c288b61f608dfdca6a80b
domeeno <dominicflocea.dev@gmail.com>
2022-10-30 21:03:07 +0200

    add claims to jwt

--- user-service/src/main/kotlin/com/pandora/userservice/utils/Utils.kt
+++ user-service/src/main/kotlin/com/pandora/userservice/utils/Utils.kt
@@ -4,11 +4,20 @@ import com.pandora.userservice.models.User
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.apache.commons.io.IOUtils
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.ResourceLoader
+import org.springframework.data.crossstore.ChangeSetPersister
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException.Unauthorized
+import org.springframework.web.server.ResponseStatusException
 import java.io.IOException
 import java.util.Date
-import kotlin.collections.HashMap
+import javax.servlet.http.HttpServletRequest
+
 
 @Value("\${spring.jwt.secret-key}")
 private val secretKey: String = "secret"
@@ -20,13 +29,16 @@ fun readResourceIntoString(resourceLoader: ResourceLoader, file: String): String
 }
 
 fun generateJwt(user: User): String {
-    val claims: HashMap<String, Any?> = HashMap<String, Any?>()
+    val claims: HashMap<String, Any?> = HashMap()
     claims["email"] = user.email
-    claims["roles"] = "user"
+    claims["userId"] = user.userId
+    claims["likes"] = user.likedId
+    claims["saves"] = user.savedId
+    claims["subjects"] = user.subjectsId
 
     return Jwts.builder()
         .setIssuer("identity")
-        .setExpiration(Date(System.currentTimeMillis() + 60 * 60 + 1000)) // hour for now
+        .setExpiration(Date(System.currentTimeMillis() + 60 * 60 + 1000)) // hour from now (for now)
         .setClaims(claims)
         .signWith(SignatureAlgorithm.HS256, secretKey)
         .compact()
